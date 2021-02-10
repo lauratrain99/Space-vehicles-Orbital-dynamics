8ea6bf97e2e3e3462eef5a42eb10580c49fd2bbb
Laura Train <laura.train@alumnos.uc3m.es>
2021-02-10 16:20:50 +0100

    rv2coe no need to specify the type of orbit

--- problem-set/problem11.m
+++ problem-set/problem11.m
@@ -13,11 +13,9 @@ v = [-3.457; 6.618; 2.533];           % km/s, ECI coords
 muE = 3.986e+5;                       % km^3/s^2
 
 %% Classical orbital elements
-[a, e, Omega, inc, omega, theta] = rv2coe_nominal(muE, r, v);
+state.r = r;
+state.v = v;
+[coe] = rv2coe(muE, state);
 
-fprintf('a = %.2f km \n',a)
-fprintf('e = %.2f \n',e)
-fprintf('Ω = %.2f º \n',Omega)
-fprintf('i = %.2f º \n',inc)
-fprintf('w = %.2f º \n',omega)
-fprintf('θ = %.2f º \n', theta)
+
+disp(coe)
